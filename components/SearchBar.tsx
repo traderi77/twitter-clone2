b72ef02d66b5f3c23bb5d853bd082a846128548f
traderi77 <lukashofer8@icloud.com>
2024-01-24 07:51:33 +0100

    small improvement

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,6 +1,13 @@
 import React, { useState } from 'react';
 import { BiSearch } from 'react-icons/bi';
-
+import { Input } from './ui/input';
+import { FaSearchLocation } from 'react-icons/fa';
+import {
+    HoverCard,
+    HoverCardContent,
+    HoverCardTrigger,
+  } from "@/components/ui/hover-card"
+  
 
 interface SearchBarProps {
     defaultSearchText?: string;
@@ -20,16 +27,26 @@ const [searchText, setSearchText] = useState(defaultSearchText);
         }
     };
     return (
-            <div className=" w-full search-bar">
-                <input
-                    type="text"
-                    value={searchText}
-                    onChange={(e) => setSearchText(e.target.value)}
-                    onKeyDown={handleKeyDown}
-                />
-                <button onClick={handleSearch}>
-                    <BiSearch />
-                </button>
+            <div className='flex flex-row w-full justify-center'>
+                <HoverCard>
+                <HoverCardTrigger>
+                    <Input 
+                        type="text"
+                        value={searchText}
+                        onChange={(e) => setSearchText(e.target.value)}
+                        onKeyDown={handleKeyDown}
+                        className='text-neutral-500 w-80'
+                        placeholder='Search for anything!'
+                        icon={<FaSearchLocation className='text-neutral-500'/>}
+                    />
+                </HoverCardTrigger>
+                <HoverCardContent className='w-80 h-120 scroll-m-0'>
+                    testgin 
+                </HoverCardContent>
+                </HoverCard>
+
+
+
             </div>
     );
 };
