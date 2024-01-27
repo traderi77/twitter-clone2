c1fcaa53772cde0c14505868b8fe333167241b8a
traderi77 <lukashofer8@icloud.com>
2024-01-27 18:22:44 +0100

    Auto commit using 'pusher'

--- components/posts/PostFeed.tsx
+++ components/posts/PostFeed.tsx
@@ -1,21 +1,24 @@
-import usePosts from '@/hooks/usePosts';
+import usePosts from "@/hooks/usePosts";
 
-import PostItem from './PostItem';
-import { truncateSync } from 'fs';
+import PostItem from "./PostItem";
+import { truncateSync } from "fs";
 
 interface PostFeedProps {
-  userId?: string,
-  fetchBookmarked?: boolean,
-  fetchLiked?: boolean, 
-  
+  userId?: string;
+  fetchBookmarked?: boolean;
+  fetchLiked?: boolean;
 }
 
-const PostFeed: React.FC<PostFeedProps> = ({ userId, fetchBookmarked, fetchLiked }) => {
+const PostFeed: React.FC<PostFeedProps> = ({
+  userId,
+  fetchBookmarked,
+  fetchLiked,
+}) => {
   const { data: posts = [] } = usePosts(userId, fetchBookmarked, fetchLiked);
 
-  return ( 
+  return (
     <>
-      {posts.map((post: Record<string, any>,) => (
+      {posts.map((post: Record<string, any>) => (
         <PostItem userId={userId} key={post.id} data={post} />
       ))}
     </>
--- components/posts/PostItem.tsx
+++ components/posts/PostItem.tsx
@@ -1,25 +1,23 @@
-import { useRouter } from 'next/router';
-import { useCallback, useMemo } from 'react';
-import { AiFillHeart, AiOutlineHeart, AiOutlineMessage } from 'react-icons/ai';
-import { formatDistanceToNowStrict } from 'date-fns';
-import useLoginModal from '@/hooks/useLoginModal';
-import useCurrentUser from '@/hooks/useCurrentUser';
-import useLike from '@/hooks/useLike';
-import useBookmark from '@/hooks/useBookmark';
-import useCitation from '@/hooks/useCitation';
-import Avatar from '../Avatar';
-import { BiBookmark, BiLink, BiRepost, BiShareAlt } from 'react-icons/bi';
-import { FaRetweet } from 'react-icons/fa';
-import usePost from '@/hooks/usePost';
+import { useRouter } from "next/router";
+import { useCallback, useMemo } from "react";
+import { AiFillHeart, AiOutlineHeart, AiOutlineMessage } from "react-icons/ai";
+import { formatDistanceToNowStrict } from "date-fns";
+import useLoginModal from "@/hooks/useLoginModal";
+import useCurrentUser from "@/hooks/useCurrentUser";
+import useLike from "@/hooks/useLike";
+import useBookmark from "@/hooks/useBookmark";
+import useCitation from "@/hooks/useCitation";
+import Avatar from "../Avatar";
+import { BiBookmark, BiLink, BiRepost, BiShareAlt } from "react-icons/bi";
+import { FaRetweet } from "react-icons/fa";
+import usePost from "@/hooks/usePost";
 import {
   HoverCard,
   HoverCardContent,
   HoverCardTrigger,
-} from "@/components/ui/hover-card"
-import PostItemDots from './PostItemDots';
-import { BsBookmarksFill } from 'react-icons/bs';
-
-
+} from "@/components/ui/hover-card";
+import PostItemDots from "./PostItemDots";
+import { BsBookmarksFill } from "react-icons/bs";
 
 interface PostItemProps {
   data: Record<string, any>;
@@ -29,86 +27,102 @@ interface PostItemProps {
 const PostItem: React.FC<PostItemProps> = ({ data = {}, userId }) => {
   const router = useRouter();
   const loginModal = useLoginModal();
-  
+
   const { data: citedPost } = usePost(data.citedId);
-  const displayData = (data.isCited === true && citedPost && citedPost.user) ? citedPost : data;
-  
+  const displayData =
+    data.isCited === true && citedPost && citedPost.user ? citedPost : data;
+
   const { data: currentUser } = useCurrentUser();
   const { hasLiked, toggleLike } = useLike({ postId: displayData.id, userId });
-  const { hasBookmarked, toggleBookmark } = useBookmark({ postId: displayData.id, userId });
-  const { hasCited, toggleCitation } = useCitation({ postId: displayData.id, userId });
+  const { hasBookmarked, toggleBookmark } = useBookmark({
+    postId: displayData.id,
+    userId,
+  });
+  const { hasCited, toggleCitation } = useCitation({
+    postId: displayData.id,
+    userId,
+  });
 
   const commentsLength = displayData.comments.length || 0;
   const likedLength = displayData.likedIds.length || 0;
   const bookmarkedLength = displayData.bookmarkedIds.length || 0;
   const citedLength = displayData.citedIds.length || 0;
 
-  const copyLinkToPost = useCallback((ev: any) => {
-    ev.stopPropagation();
+  const copyLinkToPost = useCallback(
+    (ev: any) => {
+      ev.stopPropagation();
 
-    // Construct the post link
-    const postLink = `${window.location.origin}/posts/${displayData.id}`;
+      // Construct the post link
+      const postLink = `${window.location.origin}/posts/${displayData.id}`;
 
-    // Copy the post link to the clipboard
-    navigator.clipboard.writeText(postLink);
-  }, [displayData.id]);
+      // Copy the post link to the clipboard
+      navigator.clipboard.writeText(postLink);
+    },
+    [displayData.id]
+  );
 
+  const goToUser = useCallback(
+    (ev: any) => {
+      ev.stopPropagation();
+      router.push(`/users/${displayData.user.id}`);
+    },
+    [router, displayData.user.id]
+  );
 
-  const goToUser = useCallback((ev: any) => {
-    ev.stopPropagation();
-    router.push(`/users/${displayData.user.id}`)
-  }, [router, displayData.user.id]);
-
-
-  const goToCitationUser = useCallback((ev: any) => {
-    ev.stopPropagation();
-    router.push(`/users/${data.user.id}`)
-  }, [router, data.user.id]);
-
+  const goToCitationUser = useCallback(
+    (ev: any) => {
+      ev.stopPropagation();
+      router.push(`/users/${data.user.id}`);
+    },
+    [router, data.user.id]
+  );
 
   const goToPost = useCallback(() => {
     router.push(`/posts/${displayData.id}`);
   }, [router, displayData.id]);
 
+  const onLike = useCallback(
+    async (ev: any) => {
+      ev.stopPropagation();
 
-  const onLike = useCallback(async (ev: any) => {
-    ev.stopPropagation();
-
-    if (!currentUser) {
-      return loginModal.onOpen();
-    }
-    toggleLike();
-  }, [loginModal, currentUser, toggleLike]);
-
+      if (!currentUser) {
+        return loginModal.onOpen();
+      }
+      toggleLike();
+    },
+    [loginModal, currentUser, toggleLike]
+  );
 
   const LikeIcon = hasLiked ? AiFillHeart : AiOutlineHeart;
 
-  const onCite = useCallback(async (ev: any) => {
-    ev.stopPropagation();
+  const onCite = useCallback(
+    async (ev: any) => {
+      ev.stopPropagation();
 
-    if (!currentUser) {
-      return loginModal.onOpen();
-    }
-    toggleCitation();
-  }, [loginModal, currentUser, toggleCitation]);
+      if (!currentUser) {
+        return loginModal.onOpen();
+      }
+      toggleCitation();
+    },
+    [loginModal, currentUser, toggleCitation]
+  );
 
   const CiteIcon = hasCited ? BiRepost : BiRepost;
 
-  const onBookmark = useCallback(async (ev: any) => {
-    ev.stopPropagation();
+  const onBookmark = useCallback(
+    async (ev: any) => {
+      ev.stopPropagation();
 
-    if (!currentUser) {
-      return loginModal.onOpen();
-    }
+      if (!currentUser) {
+        return loginModal.onOpen();
+      }
 
-    toggleBookmark();
-  }, [loginModal, currentUser, toggleBookmark]);
+      toggleBookmark();
+    },
+    [loginModal, currentUser, toggleBookmark]
+  );
 
   const BookmarkIcon = hasBookmarked ? BiBookmark : BiBookmark;
-
-
-
-
 
   const createdAt = useMemo(() => {
     if (!displayData?.createdAt) {
@@ -116,26 +130,30 @@ const PostItem: React.FC<PostItemProps> = ({ data = {}, userId }) => {
     }
 
     return formatDistanceToNowStrict(new Date(displayData.createdAt));
-  }, [displayData.createdAt])
+  }, [displayData.createdAt]);
 
   return (
     <div
       onClick={goToPost}
-      className={data.isCited === true && citedPost && citedPost.user ?
-        "border-b-[1px] border-neutral-800 p-5 pt-2 cursor-pointer hover:bg-neutral-900 transition"
-        :
-        " border-b-[1px] border-neutral-800 p-5 cursor-pointer hover:bg-neutral-900 transition"
-      }>
+      className={
+        data.isCited === true && citedPost && citedPost.user
+          ? "border-b-[1px] border-neutral-800 p-5 pt-2 cursor-pointer hover:bg-neutral-900 transition"
+          : " border-b-[1px] border-neutral-800 p-5 cursor-pointer hover:bg-neutral-900 transition"
+      }
+    >
       {data.isCited === true && citedPost && citedPost.user && (
         <div className="mt-0flex flex-row flex-wrap">
-          <span onClick={goToCitationUser} className="text-neutral-500 text-sm w-full flex flex-row gap-2 ml-10 hover:underline">
+          <span
+            onClick={goToCitationUser}
+            className="text-neutral-500 text-sm w-full flex flex-row gap-2 ml-10 hover:underline"
+          >
             <FaRetweet /> {data.user.name} retweeted
           </span>
         </div>
       )}
       <div className="flex flex-row items-start gap-3 w-full">
         <Avatar userId={displayData.user.id} />
-        <div className='w-full'>
+        <div className="w-full">
           <div className="flex flex-row items-center gap-2 w-full">
             <p
               onClick={goToUser}
@@ -144,7 +162,8 @@ const PostItem: React.FC<PostItemProps> = ({ data = {}, userId }) => {
                 font-semibold 
                 cursor-pointer 
                 hover:underline
-            ">
+            "
+            >
               {displayData.user.name}
             </p>
             <span
@@ -155,19 +174,24 @@ const PostItem: React.FC<PostItemProps> = ({ data = {}, userId }) => {
                 hover:underline
                 hidden
                 md:block
-            ">
+            "
+            >
               @{displayData.user.username}
             </span>
-            <span className="text-neutral-500 text-sm">
-              {createdAt} ago
-            </span>
-            <div onClick={(event) => event.preventDefault()} className='ml-auto'>
-              <PostItemDots data={(data.isCited === true && citedPost) ? citedPost : displayData} userId={displayData.user.id} />
+            <span className="text-neutral-500 text-sm">{createdAt} ago</span>
+            <div
+              onClick={(event) => event.preventDefault()}
+              className="ml-auto"
+            >
+              <PostItemDots
+                data={
+                  data.isCited === true && citedPost ? citedPost : displayData
+                }
+                userId={displayData.user.id}
+              />
             </div>
           </div>
-          <div className="text-white mt-1 w-full">
-            {displayData.body}
-          </div>
+          <div className="text-white mt-1 w-full">{displayData.body}</div>
           <div className="flex flex-row justify-between mt-3 gap-10">
             <div
               onClick={goToPost}
@@ -180,11 +204,10 @@ const PostItem: React.FC<PostItemProps> = ({ data = {}, userId }) => {
                 cursor-pointer 
                 transition 
                 hover:text-sky-500
-            ">
+            "
+            >
               <AiOutlineMessage size={24} />
-              <p>
-                {commentsLength}
-              </p>
+              <p>{commentsLength}</p>
             </div>
 
             <div
@@ -198,12 +221,13 @@ const PostItem: React.FC<PostItemProps> = ({ data = {}, userId }) => {
                 cursor-pointer 
                 transition 
                 hover:text-blue-500
-            ">
-              <CiteIcon className={hasCited ? 'text-blue-500 fill-blue-500' : ''}
-                size={24} />
-              <p>
-                {citedLength}
-              </p>
+            "
+            >
+              <CiteIcon
+                className={hasCited ? "text-blue-500 fill-blue-500" : ""}
+                size={24}
+              />
+              <p>{citedLength}</p>
             </div>
 
             <div
@@ -217,11 +241,10 @@ const PostItem: React.FC<PostItemProps> = ({ data = {}, userId }) => {
                 cursor-pointer 
                 transition 
                 hover:text-red-500
-            ">
-              <LikeIcon color={hasLiked ? 'red' : ''} size={24} />
-              <p>
-                {likedLength}
-              </p>
+            "
+            >
+              <LikeIcon color={hasLiked ? "red" : ""} size={24} />
+              <p>{likedLength}</p>
             </div>
             <div
               onClick={onBookmark}
@@ -234,16 +257,18 @@ const PostItem: React.FC<PostItemProps> = ({ data = {}, userId }) => {
                 cursor-pointer 
                 transition 
                 hover:text-blue-500
-            ">
-              <BsBookmarksFill className={hasBookmarked ? 'text-blue-500 fill-blue-500' : ''}
-                size={24} />
-              <p>
-                {bookmarkedLength}
-              </p>
+            "
+            >
+              <BsBookmarksFill
+                className={hasBookmarked ? "text-blue-500 fill-blue-500" : ""}
+                size={24}
+              />
+              <p>{bookmarkedLength}</p>
             </div>
             <HoverCard>
               <HoverCardTrigger>
-                <div className="
+                <div
+                  className="
                 flex 
                 flex-row 
                 items-center 
@@ -252,24 +277,31 @@ const PostItem: React.FC<PostItemProps> = ({ data = {}, userId }) => {
                 cursor-pointer 
                 transition 
                 hover:text-blue-500
-            ">
+            "
+                >
                   <BiShareAlt size={24} />
                 </div>
               </HoverCardTrigger>
               <HoverCardContent className="text-neutral-500 bg-black">
-                <div onClick={(event) => event.preventDefault()} className="flex flex-row w-full">
-                  <div onClick={copyLinkToPost} className='flex flex-row w-full gap-5' ><BiLink size={24} />
+                <div
+                  onClick={(event) => event.preventDefault()}
+                  className="flex flex-row w-full"
+                >
+                  <div
+                    onClick={copyLinkToPost}
+                    className="flex flex-row w-full gap-5"
+                  >
+                    <BiLink size={24} />
                     <p>Copy link to post</p>
                   </div>
                 </div>
-
               </HoverCardContent>
             </HoverCard>
           </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default PostItem;
