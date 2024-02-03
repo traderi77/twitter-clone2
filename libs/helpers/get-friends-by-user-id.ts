756853edc0827f27ac2687681c9d54aa0b5aeceb
traderi77 <lukashofer8@icloud.com>
2024-02-03 23:35:28 +0100

    zwischenstand not working yet

--- libs/helpers/get-friends-by-user-id.ts
+++ libs/helpers/get-friends-by-user-id.ts
@@ -1,21 +1,24 @@
 import { fetchRedis } from './redis'
 
 export const getFriendsByUserId = async (userId: string) => {
-  // retrieve friends for current user
-  console.log("userid", userId)
-  const friendIds = (await fetchRedis(
-    'smembers',
-    `user:${userId}:friends`
-  )) as string[]
-  console.log("friend ids", friendIds)
+  // retrieve friends for the current user
+  console.log("userId", userId);
+  
+  try {
+    const friendIds = (await fetchRedis('smembers', `user:${userId}:friends`)) as string[];
 
-  const friends = await Promise.all(
-    friendIds.map(async (friendId) => {
-      const friend = await fetchRedis('get', `user:${friendId}`) as string
-      const parsedFriend = JSON.parse(friend) as User
-      return parsedFriend
-    })
-  )
+    const friends = await Promise.all(
+      friendIds.map(async (friendId) => {
+        const friend = await fetchRedis('get', `user:${friendId}`) as string;
+        const parsedFriend = JSON.parse(friend) as User;
+        return parsedFriend;
+      })
+    );
 
-  return friends
-}
+    return friends;
+  } catch (e) {
+    console.error(e);
+    return [];
+  }
+};
+
